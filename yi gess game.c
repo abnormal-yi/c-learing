ba68065c54e5b5a1075eed2b4e226c4d3d2a3bd8
joshy Lucas <96329740+joshhy12@users.noreply.github.com>
2024-04-29 15:32:03 +0300

    Fixed indentation and formatting in level selection switch statement
    In the level selection switch statement, added missing closing braces and increased code readability by adding indentation and formatting. Also, fixed the condition in level B to check if

--- yi gess game.c
+++ yi gess game.c
@@ -11,7 +11,8 @@ const double maxmoney = 100000;
 const double minwithdraw = 200;
 const double maxwithdraw = 10000;
 
-int main() {
+int main()
+{
     // Variables
     double balance;
     char levels;
@@ -22,7 +23,6 @@ int main() {
                            "B: MEDIUM    ODD.4.50   @ 300 total game you will play",
                            "C: DIFICULT  ODD 9.50   @ 500 total game you will play"};
 
-
     printf("*******************YI********************LUCKY*******GAME*************************\n");
     printf("HELLO YOUR WELCOME\n");
     printf("**********************************************************************************\n");
@@ -30,9 +30,12 @@ int main() {
     scanf("%lf", &balance);
 
     // Check if the amount of money is within the range
-    if (balance >= minmoney && balance <= maxmoney) {
+    if (balance >= minmoney && balance <= maxmoney)
+    {
         printf("Congrat You have successfully deposited %lf\n", balance);
-    } else {
+    }
+    else
+    {
         printf("STAKE (MINIMUM 100 MAXIMUM 100000)\n");
         return 1; // Exit the program with error code
     }
@@ -49,73 +52,80 @@ int main() {
 
     levels = toupper(levels); // Convert to uppercase
 
-    switch (levels) {
-        case 'A':
-            printf("You have chosen EASY level\n");
-            printf("Guess a number between 1 and 3: ");
-            scanf("%d", &luckynum);
+    switch (levels)
+    {
+    case 'A':
+        printf("You have chosen EASY level\n");
+        printf("Guess a number between 1 and 3: ");
+        scanf("%d", &luckynum);
 
-            // Check if the guessed number is between 1 and 3
-            if (luckynum < 1 || luckynum > 3) {
-                printf("Invalid number! Choose between 1 and 3.\n");
-                return 1; // Exit the program with error code
-            }
-
-            // Seed the random number generator with the current time
-            srand(time(NULL));
-
-            // Generate and print random lucky numbers
-            for (int i = 0; i < luckynum; i++) {
-                int lucky = (rand() % 3) + 1; // Generates a random number between 1 and 3
-                printf("Lucky number %d: %d\n", i + 1, lucky);
-            }
-            break;
-
-        case 'B':
-            printf("You have chosen MEDIUM level\n");
- printf("Guess a number between 1 and 6: ");
-            scanf("%d", &luckynum);
-
-            // Check if the guessed number is between 1 and 6
-            if (luckynum < 1 || luckynum > )6 {
-                printf("Invalid number! Choose between 1 and 6.\n");
-                return 1; // Exit the program with error code
-            }
-
-            // Seed the random number generator with the current time
-            srand(time(NULL));
-
-            // Generate and print random lucky numbers
-            for (int i = 0; i < luckynum; i++) {
-                int lucky = (rand() % 6) + 1; // Generates a random number between 1 and 6
-                printf("Lucky number %d: %d\n", i + 1, lucky);
-            }
-            break;
-
-        case 'C':
-            printf("You have chosen DIFFICULT level\n");
-            printf("Guess a number between 1 and 9: ");
-            scanf("%d", &luckynum);
-
-            // Check if the guessed number is between 1 and 9
-            if (luckynum < 1 || luckynum > 3) {
-                printf("Invalid number! Choose between 1 and 9.\n");
-                return 1; // Exit the program with error code
-            }
-
-            // Seed the random number generator with the current time
-            srand(time(NULL));
-
-            // Generate and print random lucky numbers
-            for (int i = 0; i < luckynum; i++) {
-                int lucky = (rand() % 9) + 1; // Generates a random number between 1 and 9
-                printf("Lucky number %d: %d\n", i + 1, lucky);
-            }
-            break;
-
-        default:
-            printf("Invalid level choice!\n");
+        // Check if the guessed number is between 1 and 3
+        if (luckynum < 1 || luckynum > 3)
+        {
+            printf("Invalid number! Choose between 1 and 3.\n");
             return 1; // Exit the program with error code
+        }
+
+        // Seed the random number generator with the current time
+        srand(time(NULL));
+
+        // Generate and print random lucky numbers
+        for (int i = 0; i < luckynum; i++)
+        {
+            int lucky = (rand() % 3) + 1; // Generates a random number between 1 and 3
+            printf("Lucky number %d: %d\n", i + 1, lucky);
+        }
+        break;
+
+    case 'B':
+        printf("You have chosen MEDIUM level\n");
+        printf("Guess a number between 1 and 6: ");
+        scanf("%d", &luckynum);
+
+        // Check if the guessed number is between 1 and 6
+        if (luckynum < 1 || luckynum > 6)
+        {
+            printf("Invalid number! Choose between 1 and 6.\n");
+            return 1; // Exit the program with error code
+        }
+
+        // Seed the random number generator with the current time
+        srand(time(NULL));
+
+        // Generate and print random lucky numbers
+        for (int i = 0; i < luckynum; i++)
+        {
+            int lucky = (rand() % 6) + 1; // Generates a random number between 1 and 6
+            printf("Lucky number %d: %d\n", i + 1, lucky);
+        }
+        break;
+
+    case 'C':
+        printf("You have chosen DIFFICULT level\n");
+        printf("Guess a number between 1 and 9: ");
+        scanf("%d", &luckynum);
+
+        // Check if the guessed number is between 1 and 9
+        if (luckynum < 1 || luckynum > 9)
+        {
+            printf("Invalid number! Choose between 1 and 9.\n");
+            return 1; // Exit the program with error code
+        }
+
+        // Seed the random number generator with the current time
+        srand(time(NULL));
+
+        // Generate and print random lucky numbers
+        for (int i = 0; i < luckynum; i++)
+        {
+            int lucky = (rand() % 9) + 1; // Generates a random number between 1 and 9
+            printf("Lucky number %d: %d\n", i + 1, lucky);
+        }
+        break;
+
+    default:
+        printf("Invalid level choice!\n");
+        return 1; // Exit the program with error code
     }
 
     return 0;
